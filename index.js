fc6bbe9e7c8182d0560c703346e2267d42453b1d
ozel-byte <ozelcode@gmail.com>
2023-10-04 16:23:15 -0600

    New route was added

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Hola Marta')
 })
-
-app.listen(3000,()=>{ 
-    console.log("se inicio servidor");
+app.get("/cicla", (req, res) => {
+  res.send("Hello cicla, would you like to go the cinema?")
+})
+app.listen(3000, () => {
+  console.log("se inicio servidor");
 })
